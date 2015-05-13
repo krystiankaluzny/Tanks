29a8f7500a49443c8d2a4ffbb4a09e5cb1cf419e
Krystian Kałużny <krystian.kaluzny@logicit.eu>
2015-05-13 20:01:50 +0200

    Dokumentacja klasy SproteConfig i App.

--- src/app.h
+++ src/app.h
@@ -3,16 +3,37 @@
 
 #include "app_state/appstate.h"
 
+/**
+ * @brief
+ * Klasa główna aplikacji. Jej działanie jest równoważne z działaniem aplikacji.
+ */
 class App
 {
 public:
     App();
     ~App();
+    /**
+     * Funkcja zawiera inicjalizację biblioteki SDL2, silnika gry, wczytanie tekstur oraz czcionek.
+     * Po poprawnej inicjalizacji następuje wejście w pętlę główną programu, która kolejno: reaguje na zdarzenie,
+     * uaktualnia obecny stan aplikacji, rysuje obiety na ekranie.
+     */
     void run();
+    /**
+     * Funkcja obsługująca zdarzenia czekające w kolejce.
+     */
     void eventProces();
 private:
+    /**
+     * Zmienna podtrzymująca działanie pętli głównej programu.
+     */
     bool is_running;
+    /**
+     * Aktualny stan aplikacji.
+     */
     AppState* m_app_state;
+    /**
+     * Obiekt okna aplikacji.
+     */
     SDL_Window* m_window;
 };
 
--- src/engine/spriteconfig.h
+++ src/engine/spriteconfig.h
@@ -5,26 +5,64 @@
 #include <map>
 #include <SDL2/SDL.h>
 
+/**
+ * @brief
+ * Struktura przechowująca informacjie danym typie animacji obiektu.
+ */
 struct SpriteData
 {
     SpriteData() { rect.x = 0; rect.y = 0; rect.w = 0; rect.h = 0; frames_count = 0; frame_duration = 0; loop = false;}
     SpriteData(int x, int y, int w, int h, int fc, int fd, bool l)
         { rect.x = x; rect.y = y; rect.w = w; rect.h = h; frames_count = fc; frame_duration = fd; loop = l;}
 
+    /**
+     * Pozycja i wymiary pierwszej klatki animacji
+     */
     SDL_Rect rect;
+    /**
+     * Liczba klatek w animacji.
+     */
     int frames_count;
-
-    unsigned frame_duration; //czas wyświetlania jednej klatki ms
+    /**
+     * Czas wyświetlania jednej klatki milisekundach.
+     */
+    unsigned frame_duration;
+    /**
+     * Zmienna określająca czy animacja jest zapętlona.
+     */
     bool loop;
 };
 
+/**
+ * @brief
+ * Klasa przechowuje informacjie o wszystkich typach animacji w grze.
+ */
 class SpriteConfig
 {
 public:
     SpriteConfig();
+    /**
+     * Pobranie wybranego typu animacji.
+     * @param sp - szukany typ animacji
+     * @return animację danego typu
+     */
     const SpriteData* getSpriteData(SpriteType sp) const;
 private:
+    /**
+     * Kontener przechowujący wszystkie typy animacji.
+     */
     std::map<SpriteType, SpriteData> m_configs;
+    /**
+     * Funkcja używana przy dodawaniu nowego typu animacji.
+     * @param st - typ animacji
+     * @param x - połoźenie poziome pierwszej klatki animacji w teksturze
+     * @param y - połoźenie pionowe pierwszej klatki animacji w teksturze
+     * @param w - szerokości klatek animacji
+     * @param h - wysokości klatek animacji
+     * @param fc - liczba klatek w animacji
+     * @param fd - czas wyświetlania jednej klatki
+     * @param l - zapętlenie animacji
+     */
     void insert(SpriteType st , int x, int y, int w, int h, int fc, int fd, bool l);
 };
 
