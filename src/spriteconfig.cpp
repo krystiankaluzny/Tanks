673e0744588cdfe53d84b005782b216bb540a2bd
Krystian Kałużny <k.kaluzny141@gmail.com>
2025-12-01 19:23:26 +0100

    Refactor sprite configuration; reintroduce SpriteConfig class and update sprite data handling

---
+++ src/spriteconfig.cpp
@@ -0,0 +1,54 @@
+#include "spriteconfig.h"
+
+SpriteConfig::SpriteConfig()
+{
+    insert(ST_TANK_A, {128, 0, 32, 32}, 2, 100, true);
+    insert(ST_TANK_B, {128, 64, 32, 32}, 2, 100, true);
+    insert(ST_TANK_C, {128, 128, 32, 32}, 2, 100, true);
+    insert(ST_TANK_D, {128, 192, 32, 32}, 2, 100, true);
+
+    insert(ST_PLAYER_1, {640, 0, 32, 32}, 2, 50, true);
+    insert(ST_PLAYER_2, {768, 0, 32, 32}, 2, 50, true);
+
+    insert(ST_BRICK_WALL, {928, 0, 16, 16}, 1, 200, false);
+    insert(ST_STONE_WALL, {928, 144, 16, 16}, 1, 200, false);
+    insert(ST_WATER, {928, 160, 16, 16}, 2, 350, true);
+    insert(ST_BUSH, {928, 192, 16, 16}, 1, 200, false);
+    insert(ST_ICE, {928, 208, 16, 16}, 1, 200, false);
+
+    insert(ST_BONUS_GRENADE, {896, 0, 32, 32}, 1, 200, false);
+    insert(ST_BONUS_HELMET, {896, 32, 32, 32}, 1, 200, false);
+    insert(ST_BONUS_CLOCK, {896, 64, 32, 32}, 1, 200, false);
+    insert(ST_BONUS_SHOVEL, {896, 96, 32, 32}, 1, 200, false);
+    insert(ST_BONUS_TANK, {896, 128, 32, 32}, 1, 200, false);
+    insert(ST_BONUS_STAR, {896, 160, 32, 32}, 1, 200, false);
+    insert(ST_BONUS_GUN, {896, 192, 32, 32}, 1, 200, false);
+    insert(ST_BONUS_BOAT, {896, 224, 32, 32}, 1, 200, false);
+
+    insert(ST_SHIELD, {976, 0, 32, 32}, 2, 45, true);
+    insert(ST_CREATE, {1008, 0, 32, 32}, 10, 100, false);
+    insert(ST_DESTROY_TANK, {1040, 0, 64, 64}, 7, 70, false);
+    insert(ST_DESTROY_BULLET, {1108, 0, 32, 32}, 5, 40, false);
+    insert(ST_BOAT_P1, {944, 96, 32, 32}, 1, 200, false);
+    insert(ST_BOAT_P2, {976, 96, 32, 32}, 1, 200, false);
+
+    insert(ST_EAGLE, {944, 0, 32, 32}, 1, 200, false);
+    insert(ST_DESTROY_EAGLE, {1040, 0, 64, 64}, 7, 100, false);
+    insert(ST_FLAG, {944, 32, 32, 32}, 1, 200, false);
+
+    insert(ST_BULLET, {944, 128, 8, 8}, 1, 200, false);
+    insert(ST_LEFT_ENEMY, {944, 144, 16, 16}, 1, 200, false);
+    insert(ST_STAGE_STATUS, {976, 64, 32, 32}, 1, 200, false);
+
+    insert(ST_TANKS_LOGO, {0, 260, 406, 72}, 1, 200, false);
+}
+
+const SpriteData &SpriteConfig::getSpriteData(SpriteType st) const
+{
+    return m_configs.at(st);
+}
+
+void SpriteConfig::insert(SpriteType st, Rect rect, int frames_count, unsigned frame_duration, bool loop)
+{
+    m_configs[st] = SpriteData{rect, frames_count, frame_duration, loop};
+}
---
+++ src/spriteconfig.h
@@ -0,0 +1,118 @@
+#ifndef SPRITECONFIG_H
+#define SPRITECONFIG_H
+
+#include "data/data.h"
+#include <map>
+
+enum SpriteType
+{
+    ST_TANK_A,
+    ST_TANK_B,
+    ST_TANK_C,
+    ST_TANK_D,
+
+    ST_PLAYER_1,
+    ST_PLAYER_2,
+
+    ST_BRICK_WALL,
+    ST_STONE_WALL,
+    ST_WATER,
+    ST_BUSH,
+    ST_ICE,
+
+    ST_BONUS_GRENADE,
+    ST_BONUS_HELMET,
+    ST_BONUS_CLOCK,
+    ST_BONUS_SHOVEL,
+    ST_BONUS_TANK,
+    ST_BONUS_STAR,
+    ST_BONUS_GUN,
+    ST_BONUS_BOAT,
+
+    ST_SHIELD,
+    ST_CREATE,
+    ST_DESTROY_TANK,
+    ST_DESTROY_BULLET,
+    ST_BOAT_P1,
+    ST_BOAT_P2,
+
+    ST_EAGLE,
+    ST_DESTROY_EAGLE,
+    ST_FLAG,
+
+    ST_BULLET,
+
+    ST_LEFT_ENEMY,
+    ST_STAGE_STATUS,
+
+    ST_TANKS_LOGO,
+
+    ST_NONE
+};
+
+/**
+ * @brief
+ * Structure storing information about a given object animation type.
+ */
+struct SpriteData
+{
+    /**
+     * Position and dimensions of the first animation frame
+     */
+    Rect rect;
+    /**
+     * Number of frames in the animation.
+     */
+    int frames_count;
+    /**
+     * Display time of one frame in milliseconds.
+     */
+    unsigned frame_duration;
+    /**
+     * Variable determining whether the animation is looped.
+     */
+    bool loop;
+};
+
+/**
+ * @brief
+ * Class that stores information about all animation types in the game.
+ */
+class SpriteConfig
+{
+public:
+    static SpriteConfig &getInstance()
+    {
+        static SpriteConfig instance;
+        return instance;
+    }
+
+    /**
+     * Get the selected animation type.
+     * @param sp - requested animation type
+     * @return animation of the given type
+     */
+    const SpriteData &getSpriteData(SpriteType sp) const;
+
+private:
+    SpriteConfig();
+    ~SpriteConfig() = default;
+    SpriteConfig(const SpriteConfig &) = delete;
+    SpriteConfig &operator=(const SpriteConfig &) = delete;
+
+    /**
+     * Container storing all animation types.
+     */
+    std::map<SpriteType, SpriteData> m_configs;
+    /**
+     * Function used when adding a new animation type.
+     * @param st - animation type
+     * @param rect - position and dimensions of the first animation frame in the texture
+     * @param frames_count - number of frames in the animation
+     * @param frame_duration - display time of one frame
+     * @param loop - animation looping flag
+     */
+    void insert(SpriteType st, Rect rect, int frames_count, unsigned frame_duration, bool loop);
+};
+
+#endif // SPRITECONFIG_H
