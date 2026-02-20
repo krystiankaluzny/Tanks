682ce0bc6c1b2b97fb234007ee7e7d98737aee94
Redas Jefisovas <114405740+holoflash@users.noreply.github.com>
2026-02-20 14:05:00 +0100

    new respawn sound+better existing

--- src/soundconfig.h
+++ src/soundconfig.h
@@ -9,8 +9,8 @@ namespace SoundConfig
     const Sound PAUSE{"sounds/pause.wav", 100.0, 1.0, false};
     const Sound GAME_OVER{"sounds/game_over.wav", 100.0, 1.0, false};
 
-    const Sound BONUS_APPEARED{"sounds/bonus_appeared.wav", 100.0, 0.9, true};
-    const Sound BONUS_OBTAINED{"sounds/bonus_obtained.wav", 100.0, 0.9, true};
+    const Sound BONUS_APPEARED{"sounds/bonus_appeared.wav", 90.0, 0.9, true};
+    const Sound BONUS_OBTAINED{"sounds/bonus_obtained.wav", 90.0, 0.9, true};
 
     const Sound BULLET_HIT_BRICK{"sounds/bullet_hit_brick.wav", 40.0, 0.7, true};
     const Sound BULLET_HIT_MAP_BOUNDARIES{"sounds/bullet_hit_map_boundaries.wav", 40.0, 1.0, true};
